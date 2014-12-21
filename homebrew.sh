02a350f2f648dedd71fbdedd44a2b115d0c6e54c
Martin Ledgard <martin@le6o.com>
2014-12-21 23:23:01 +0000

    initial homebrew script

---
+++ homebrew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+which -s brew && brew update
+which -s brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew upgrade
+
+formulas=(
+  bash-completion
+  git
+  git-flow
+  php56
+  composer
+)
+
+for formula in "${formulas[@]}"; do
+  brew install $formula || brew upgrade $formula
+done
+
+brew cleanup
