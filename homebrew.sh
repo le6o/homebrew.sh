9ff05fd4d36ab470a87066d170c9172eabb67474
Martin Ledgard <martin@le6o.com>
2015-03-01 22:36:29 +0000

    add casks

--- homebrew.sh
+++ homebrew.sh
@@ -10,10 +10,27 @@ formulas=(
   git
   php56
   composer
+  caskroom/cask/brew-cask
 )
 
 for formula in "${formulas[@]}"; do
   brew install $formula || brew upgrade $formula
 done
 
+casks=(
+  dropbox
+  google-chrome
+  slack
+  sublime-text3
+  vagrant
+  virtualbox
+)
+
+brew tap caskroom/versions
+
+for cask in "${casks[@]}"; do
+  brew cask install --appdir="/Applications" $cask
+done
+
 brew cleanup
+brew cask cleanup
